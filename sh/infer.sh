3dac45167f394efb09b2e969608845bd6f44569e
toriving <toriving@gmail.com>
2020-08-23 19:47:14 +0900

    Update project

---
+++ sh/infer.sh
@@ -0,0 +1,8 @@
+python main.py \
+        --do_predict \
+        --output_dir infer_out \
+        --data_dir data_in \
+        --cache_dir .cache \
+        --overwrite_output_dir \
+        --model albert_data_out \
+        --per_device_eval_batch_size 96
---
+++ sh/train.sh
@@ -0,0 +1,25 @@
+python main.py \
+        --do_eval \
+        --do_train \
+        --do_predict \
+        --evaluate_during_training \
+        --output_dir data_out \
+        --data_dir data_in \
+        --cache_dir .cache \
+        --overwrite_output_dir \
+        --model albert-xxlarge-v2 \
+        --seed 2020 \
+        --save_total_limit 3 \
+        --learning_rate 3e-5 \
+        --per_device_train_batch_size 4 \
+        --per_device_eval_batch_size 96 \
+        --num_train_epochs 2.0 \
+        --max_seq_length 512 \
+        --eval_steps 125 \
+        --logging_steps 13 \
+        --save_steps 125 \
+        --gradient_accumulation_steps 8 \
+        --warmup_steps 500 \
+        --fp16 \
+        --fp16_opt_level O1 \
+        --dynamic_doc_masking
